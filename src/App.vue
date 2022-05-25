2b20acfd81632ff7ed1beba5d3946dc76de35f39
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-25 12:04:08 +0900

    セクション8のスロットが終わったので

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,38 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png"/>
-    <p>親に渡されたlikeCount({{counterName}})</p>
-    <LikeCounter
-      propsLikeCount=5
+    <like-header propsName="tekitou">
+      <p>デフォルトスロット用</p>
+      <h4>デフォルトスロット用</h4>
+      <template v-slot:title>
+        <h2>hello</h2>
+      </template>
+      <template v-slot:tekitou>
+        <h1>親に渡されたlikeCount({{counterName}})</h1>
+      </template>
+      <template v-slot:user="slotProps">
+        <h2>{{slotProps}}</h2>
+        <h2>{{slotProps.user.firstname}} {{slotProps.user.middlename}} {{slotProps.user.lastname}}</h2>
+      </template>
+
+    </like-header>
+    <like-counter
+      :propsLikeCount=5
       propsName="likeCounterA"
       @my-click="changeCounterName"
     >
-    </LikeCounter>
-    <LikeCounter
-      propsLikeCount=0
+    </like-counter>
+    <like-counter
+      :propsLikeCount=0
       propsName="likeCounterB"
       @my-click="changeCounterName"
-    ></LikeCounter>
+    ></like-counter>
   </div>
 </template>
 
 <script>
 import LikeCounter from './components/LikeCounter.vue';
+import LikeHeader from './components/likeHeader.vue';
 export default {
   name: 'App',
   data(){
@@ -26,7 +41,8 @@ export default {
     }
   },
   components: {
-    LikeCounter
+    LikeCounter,
+    LikeHeader
   },
   methods:{
     changeCounterName(arg){
---
+++ src/components/likeHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <slot name="title"></slot>
+    <p>渡された文字列[{{name}}]</p>
+    <slot name="tekitou">
+        <h1>何も渡されなかったときに表示される文字列</h1>
+    </slot>
+    <slot name="user" :user="user"></slot>
+    <slot>デフォルトスロット</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          user:{
+              firstname:"jack",
+              middlename:" o ",
+              lastname:"lantan"
+          }
+        }
+  },
+  props: {
+      propsName:{
+          type:String,
+          default:"none"
+      },
+  },
+  computed:{
+      
+  },
+  methods: {
+      
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+    color: darkcyan;
+}
+</style>
